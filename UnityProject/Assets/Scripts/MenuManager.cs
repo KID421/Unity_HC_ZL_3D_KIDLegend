4142b193521c14eda8d4b8de3a1669dfd135cf63
KID <50666207+KID421@users.noreply.github.com>
2020-02-25 19:37:28 +0800

    完成內購設定

---
+++ UnityProject/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("玩家資料")]
+    public PlayerData dataPlayer;
+
+    public void BuyHp_500()
+    {
+        dataPlayer.hpMax += 500;            // 最大值遞增
+        dataPlayer.hp = dataPlayer.hpMax;
+    }
+
+    public void BuyAtk_50()
+    {
+        dataPlayer.attack += 50;            // 攻擊力遞增
+    }
+
+    public void LoadLevel()
+    {
+        dataPlayer.hp = dataPlayer.hpMax;
+        SceneManager.LoadScene("關卡 1");
+    }
+}
